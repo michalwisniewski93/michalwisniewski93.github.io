3a803977ea814eaeb32ec3ac721a0a93b43ddf06
Michał Wiśniewski Front End Developer <michalwisniewskifrontend@onet.pl>
2025-01-23 19:37:00 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,7 +2,7 @@ const iconMenu = document.querySelector('.menuIco')
 const menuSection = document.querySelector('section.menu')
 const sectionmenunavul = document.querySelector('section.menu nav ul')
 
-iconMenu.addEventListener('click', function(){
+iconMenu.addEventListener('touchstart', function(){
     menuSection.classList.toggle('show')
     sectionmenunavul.classList.toggle('show')
 })
